e0fe84ab24d9d5214aef6175f91f1390e326f690
Kyle Johnson <59038857+kylejohnson1985@users.noreply.github.com>
2020-04-26 11:05:41 -0400

    Initial Commit

---
+++ Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        public string Text { get; set; }
+        public string Author { get; set; }
+        public Post CommentPost { get; set; }
+    }
+}
