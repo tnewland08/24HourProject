0d8db9c7c9b27a04a42355a3424b84cbf256523e
J-Crawford89 <60453593+J-Crawford89@users.noreply.github.com>
2020-04-28 02:22:59 -0400

    comment entity - not quite working

---
+++ API/Controllers/CommentController.cs
@@ -0,0 +1,59 @@
+﻿using Data;
+using Microsoft.AspNet.Identity;
+using Models.CommentModels;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class CommentController : ApiController
+    {
+        private CommentService CreateCommentService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var commentService = new CommentService(userId);
+            return commentService;
+        }
+        [HttpPost]
+        //[Route(Name ="AddComment")]
+        public IHttpActionResult Post(CommentCreateModel commentToCreate)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateCommentService();
+            service.AddComment(commentToCreate);
+            return Ok();
+        }
+        [HttpGet]
+        //[Route(Name ="GetPostComments")]
+        public IHttpActionResult Get(int postId)
+        {
+            var service = CreateCommentService();
+            var comments = service.GetCommentsByPost(postId);
+            return Ok(comments);
+        }
+        [HttpPut]
+        //[Route(Name ="UpdateComment")]
+        public IHttpActionResult Put(CommentUpdateModel commentToUpdate)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateCommentService();
+            service.UpdateComment(commentToUpdate);
+            return Ok();
+        }
+        [HttpDelete]
+        //[Route(Name ="RemoveComment")]
+        public IHttpActionResult Delete(CommentDeleteModel commentToDelete)
+        {
+            var service = CreateCommentService();
+            service.RemoveComent(commentToDelete);
+            return Ok();
+        }
+    }
+}
--- Data/IdentityModels.cs
+++ Data/IdentityModels.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
 using System.Threading.Tasks;
