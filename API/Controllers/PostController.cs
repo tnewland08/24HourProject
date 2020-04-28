a47c70dfe9bec38bfdc3b826d3bb163605f47c2b
J-Crawford89 <60453593+J-Crawford89@users.noreply.github.com>
2020-04-28 18:47:41 -0400

    added post

---
+++ API/Controllers/PostController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNet.Identity;
+using Models.PullModels;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    [RoutePrefix("api/posts")]
+    public class PostController : ApiController
+    {
+        private PostService CreatePostService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var postService = new PostService(userId);
+            return postService;
+        }
+        [HttpPost]
+        [Route("addpost")]
+        public IHttpActionResult Post(PostCreateModel postToCreate)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreatePostService();
+            service.CreatePost(postToCreate);
+            return Ok();
+        }
+        [HttpGet]
+        [Route("postlist")]
+        public IHttpActionResult Get()
+        {
+            var service = CreatePostService();
+            var posts = service.GetPosts();
+            return Ok(posts);
+        }
+        [HttpPut]
+        [Route("update")]
+        public IHttpActionResult Put(PostUpdateModel postToUpdate)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreatePostService();
+            service.UpdatePost(postToUpdate);
+            return Ok();
+        }
+        [HttpDelete]
+        [Route("{id:int}")]
+        public IHttpActionResult Delete(PostDeleteModel postToDelete)
+        {
+            var service = CreatePostService();
+            service.RemovePost(postToDelete);
+            return Ok();
+        }
+    }
+}
---
+++ Services/PostService.cs
@@ -0,0 +1,59 @@
+﻿using Data;
+using Models.PullModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class PostService
+    {
+        private readonly ApplicationDbContext _ctx = new ApplicationDbContext();
+        private readonly Guid _userId;
+        public PostService(Guid userId)
+        {
+            _userId = userId;
+        }
+        public void CreatePost(PostCreateModel postToCreate)
+        {
+            var entity = new Post()
+            {
+                UserId = _userId,
+                Text = postToCreate.Text,
+                Title = postToCreate.Title
+            };
+            _ctx.Posts.Add(entity);
+            _ctx.SaveChanges();
+        }
+        public IEnumerable<PostListItem> GetPosts()
+        {
+            var returnList = _ctx.Posts.Select(e => new PostListItem
+            {
+                Title = e.Title,
+                Text = e.Text,
+                AuthorName = e.Author.Name
+            }).ToList();
+            return returnList;
+        }
+        public void UpdatePost(PostUpdateModel postToUpdate)
+        {
+            var entity = _ctx.Posts.Single(e => e.PostId == postToUpdate.PostId);
+            if (entity != null)
+            {
+                if (postToUpdate.UpdatedText != null)
+                    entity.Text = postToUpdate.UpdatedText;
+                if (postToUpdate.UpdatedTitle != null)
+                    entity.Title = postToUpdate.UpdatedTitle;
+                _ctx.SaveChanges();
+            }
+        }
+        public void RemovePost(PostDeleteModel postToDelete)
+        {
+            var entity = _ctx.Posts.Single(e => e.PostId == postToDelete.PostId);
+            _ctx.Posts.Remove(entity);
+            _ctx.SaveChanges();
+        }
+    }
+}
